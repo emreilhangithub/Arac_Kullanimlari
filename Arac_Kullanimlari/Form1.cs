b2c7b787869be99c95ea3efc8c32ecba1242d6f1
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2021-05-24 11:31:17 +0300

    merhaba dünya yazdık

--- Arac_Kullanimlari/Form1.cs
+++ Arac_Kullanimlari/Form1.cs
@@ -8,13 +8,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Arac_Kullanimlari
+namespace Arac_Kullanimlari //Proje ismimiz burada belirtiriz 
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //butona tıklanınca ne olsun
+        {
+            MessageBox.Show("Merhaba Dünya");
         }
     }
 }
